28ce77ee1ddb3cb02fdb200b9f4fdc11b2b4f616
Paul Mendoza <mendoza.paul@gmail.com>
2012-10-13 21:51:10 -0700

    Updated RecentPolls page to work again.

--- Site/Site/Models/Home/RecentPollsViewModel.cs
+++ Site/Site/Models/Home/RecentPollsViewModel.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Objects;
+using UptimeData;
 
 namespace Site.Models.Home
 {
     public class RecentPollsViewModel
     {
-        public List<UptimeData.PollCategoryValue> PollValues { get; set; }
+        public class ValueItem
+        {
+            public DateTimeOffset Time { get; set; }
+            public string Region { get; set; }
+            public string Server { get; set; }
+            public PollStatusType Status { get; set; }
+        }
+
+        public List<ValueItem> PollValues { get; set; }
 
         public RecentPollsViewModel(UptimeData.UptimeDB db)
         {
-            var recentPolls = db.GetMostRecentValues(200);
+            var recentPolls = db.PollCategoryValue
+                .OrderByDescending(p => p.CreatedTime)
+                .Take(200)
+                .Select(v => new ValueItem()
+                {
+                    Region = v.PollCategory.Region,
+                    Server = v.PollCategory.ServerCategory,
+                    Status = v.Status,
+                    Time = v.CreatedTime
+                });
             PollValues = recentPolls.ToList();
         }
 
